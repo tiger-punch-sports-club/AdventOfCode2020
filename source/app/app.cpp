c0999801c2b869ae41bd1f792a5dcf81f52158b8
Sergej Schefer <sergej.schefer@web.de>
2020-12-01 10:35:47 +0100

    test

--- source/app/app.cpp
+++ source/app/app.cpp
@@ -5,7 +5,9 @@
 #else
 #include <Python.h>
 #endif
+#include <functional>
 #include <pybind11/embed.h>
+#include <pybind11/stl.h>
 
 #include <stdio.h>
 
@@ -39,16 +41,36 @@ PYBIND11_EMBEDDED_MODULE (embeddedmodule, module)
 int main ()
 {
     pybind11::scoped_interpreter guard{};
+    const auto test_python = pybind11::module::import ("test");
 
-    auto sys = pybind11::module::import ("sys");
-    pybind11::print (sys.attr ("path"));
+    const std::function<pybind11::object(const char*)> load_file = test_python.attr("load_file");
+    const auto result = load_file("test.txt");
 
-    MyData data {};
-    data.x = 33.0f;
+    auto file_content = result.cast<std::vector<std::string>>();
 
-    auto py_module = pybind11::module::import ("python_module");
-    auto test_python = pybind11::module::import ("test");
-           
-    printf ("end\n");
+    int* numbers = static_cast<int*>(malloc(file_content.size() * sizeof(int)));
+    int numbers_count = 0;
+    for (auto& number_str : file_content)
+    {
+        numbers[numbers_count++] = std::stoi(number_str);
+    }
+    
+
+    // {
+    //     std::function<void(int)> XxX = std::function<void(int)>(load_file);
+    //     std::function<void(const char*)> yyy = std::function<void(const char*)>(load_file);
+    //     std::function<pybind11::object(int)> zzz = std::function<pybind11::object(int)>(load_file);
+    //
+    //     XxX(5);
+    //     yyy("hip hip hura");
+    //     auto res = zzz(55).cast<int>();
+    //     printf("RES: %d\n", res);    
+    //     auto result = load_file("Hello World!!");
+    //     auto test = pybind11::str(result);
+    //     std::string tt(test);
+    //     printf ("Result: %s \n", tt.c_str());
+    // }
+
+    free(numbers);
     return 0;
 }
--- source/app/test.py
+++ source/app/test.py
@@ -1,1 +1,4 @@
-﻿print("Hello Python!")
+﻿def load_file(file_path):
+    file = open(file_path, 'r') 
+    lines = file.readlines()
+    return lines
